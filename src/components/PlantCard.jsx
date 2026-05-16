c5b20ff687211528ca96521b3518b29461d38955
lennymethu-sys <lenny.methu@student.moringaschool.com>
2026-05-16 20:32:34 +0300

    add data-testid to PlantCard

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PlantCard({ plant, onToggleStock }) {
   return (
-    <li className="card">
+    <li data-testid="plant-item" className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: ${plant.price}</p>
